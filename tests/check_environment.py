1fea432c045fabe5cfe967e35302d4fe0d0897cb
stefian <stefan.ianta@w3ai.net>
2019-07-27 12:35:00 -0400

    added @mark.skip for qa and stage tests

--- tests/check_environment.py
+++ tests/check_environment.py
@@ -1,3 +1,6 @@
+from pytest import mark
+
+@mark.skip
 def check_environment_is_qa(app_config):
     base_url = app_config.base_url
     port = app_config.app_port
@@ -7,6 +10,11 @@ def check_environment_is_qa(app_config):
 def check_environment_is_dev(app_config):
     base_url = app_config.base_url
     port = app_config.app_port
-    driver.get(base_url)
-    # assert base_url == 'https://mydev-env.com'
-    # assert port == 8080
+    # driver.get(base_url)
+    assert base_url == 'https://mydev-env.com'
+    assert port == 8080
+
+@mark.skip
+def check_environment_is_stage(app_config):
+    base_url = app_config.base_url
+    assert base_url == 'stage'
