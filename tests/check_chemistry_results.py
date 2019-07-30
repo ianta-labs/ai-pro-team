64ee1e89462f77ace46a9412e37981ec6562d6e7
stefian <stefan.ianta@w3ai.net>
2019-07-30 09:17:47 -0300

    experiment checking / testing with 8 threads

--- tests/check_chemistry_results.py
+++ tests/check_chemistry_results.py
@@ -15,3 +15,19 @@ def check_result_3_completes_as_expected():
 def check_result_4_completes_as_expected():
     time.sleep(5)
     print("Result 4 has completed")
+
+def check_result_5_completes_as_expected():
+    time.sleep(5)
+    print("Result 5 has completed")
+
+def check_result_6_completes_as_expected():
+    time.sleep(5)
+    print("Result 6 has completed")
+
+def check_result_7_completes_as_expected():
+    time.sleep(5)
+    print("Result 7 has completed")
+
+def check_result_8_completes_as_expected():
+    time.sleep(5)
+    print("Result 8 has completed")
